121b3b461426c7ef639673a022f88f6b277df489
evapetrovska <E.Petrovska@acg.edu>
2025-12-11 00:59:57 +0200

    Fixed the URL LINK.

--- disneystore/disneystore/settings.py
+++ disneystore/disneystore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [https://itc4214disneyproject-1.onrender.com/]
+ALLOWED_HOSTS = ['itc4214disneyproject-1.onrender.com/']
 
 
 # Application definition
